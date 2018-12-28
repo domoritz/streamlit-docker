ad2bfb16be9ed3d2932e6601ae1023be26e08e40
Dominik Moritz <domoritz@gmail.com>
2018-12-28 14:47:38 -0800

    initial import

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Streamlit in Docker
+
+Run `docker-compose run streamlit run src/main.py`
---
+++ src/main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('My first Streamlit report')
+st.write('Hello, **Streamlit**!')
