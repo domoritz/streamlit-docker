c4c8bc19c37c270168d6f2b1312d0ba71e27cb76
Dominik Moritz <domoritz@gmail.com>
2018-12-30 14:21:26 -0800

    empty -> id transform

--- src/main.py
+++ src/main.py
@@ -5,26 +5,39 @@ from vega_datasets import data
 st.title("Streamlit with Altair")
 
 cars = data.cars()
+iris = data.iris()
 
-chart = alt.Chart(cars).mark_point().encode(
+cars_scatter = alt.Chart(cars).mark_point().encode(
     x="Horsepower",
     y="Miles_per_Gallon",
     color="Origin",
-).interactive()
+)
 
-def empty(data):
-    """ Altair data transformer that returns an empty array. """
+iris_scatter = alt.Chart(iris).mark_circle().encode(
+    alt.X('sepalLength', scale=alt.Scale(zero=False)),
+    alt.Y('sepalWidth', scale=alt.Scale(zero=False)),
+    color='species'
+)
+
+chart = cars_scatter | iris_scatter
+
+def id_transform(data):
+    """ Altair data transformer that returns a fake named dataset with the object id. """
     return {
-        "values": []
+        "name": str(id(data))
     }
 
-# register the empty transformer
-alt.data_transformers.register("empty", empty)
+# register the id transformer
+alt.data_transformers.register("id", id_transform)
 
-with alt.data_transformers.enable("empty"):
+with alt.data_transformers.enable("id"):
     chart_dict = chart.to_dict()
-    chart_dict.pop("datasets", None)
 
     st.json(chart_dict)
 
-    st.vega_lite_chart(cars, chart_dict)
+    data = {
+        id(cars): cars,
+        id(iris): iris
+    }
+
+    st.vega_lite_chart(data, chart_dict)
